4013f8c114e812812b3c25b83fe6390e82950b98
Abhishek Kumar Mehra <abhishekmehra9406@gmail.com>
2026-04-28 08:37:45 +0530

    Update db.py

--- db.py
+++ db.py
@@ -7,10 +7,10 @@ load_dotenv()
 def get_connection():
     try:
         conn = pymysql.connect(
-            host=os.getenv("DB_HOST", "localhost"),
-            user=os.getenv("DB_USER", "root"),
-            password=os.getenv("DB_PASS", "12345678"),
-            database=os.getenv("DB_NAME", "chatdb"),
+            host=os.environ["DB_HOST"],
+            user=os.environ["DB_USER"],
+            password=os.environ["DB_PASS"],
+            database=os.environ["DB_NAME"],
             cursorclass=pymysql.cursors.DictCursor,
             autocommit=True
         )
@@ -19,20 +19,20 @@ def get_connection():
         print("DB Connection Error:", e)
         return None
 
+
 def execute(query, params=None, fetch=False):
-    """
-    execute a query
-    fetch=True -> return fetched rows (SELECT)
-    fetch=False -> return lastrowid (INSERT/UPDATE/DELETE)
-    """
     conn = get_connection()
     if not conn:
         return None
+
     try:
         with conn.cursor() as cur:
             cur.execute(query, params or ())
             if fetch:
                 return cur.fetchall()
             return cur.lastrowid
+    except Exception as e:
+        print("Query Error:", e)
+        return None
     finally:
         conn.close()
